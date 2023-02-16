c93e3ca323eb4c819ceed24cd0b5ef90c7c5a421
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-02-16 06:27:40 +0300

    new_bd

--- бд2.py
+++ бд2.py
@@ -5,7 +5,8 @@ from datetime import date
 
 import sys
 from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox
-from PySide2.QtWidgets import *
+from PySide2.QtWidgets  import *
+from PyQt5 import uic
 
 
 class Ui_Form(object):
@@ -137,44 +138,70 @@ class AddRaceWidget(QMainWindow, Ui_Form):  # окно добавления да
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import *
+from PySide2.QtCore   import *
 from PySide2.QtGui import *
-from PySide2.QtWidgets import *
+from PySide2.QtCore  import *
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1222, 711)
-        self.exitAction = QAction(MainWindow)
-        self.exitAction.setObjectName(u"exitAction")
         self.centralwidget = QWidget(MainWindow)
-        self.centralwidget.setObjectName(u"centralwidget")
-        self.addFilmButton = QPushButton(self.centralwidget)
-        self.addFilmButton.setObjectName(u"addFilmButton")
-        self.addFilmButton.setGeometry(QRect(20, 20, 151, 51))
-        self.deleteFilmButton = QPushButton(self.centralwidget)
-        self.deleteFilmButton.setObjectName(u"deleteFilmButton")
-        self.deleteFilmButton.setGeometry(QRect(190, 20, 151, 51))
-        self.filmsTable = QTableWidget(self.centralwidget)
-        self.filmsTable.setObjectName(u"filmsTable")
-        self.filmsTable.setGeometry(QRect(20, 100, 1181, 571))
+        self.centralwidget.setObjectName("centralwidget")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setGeometry(QRect(0, 0, 1291, 711))
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName("tab")
+        self.deleteFilmButton = QPushButton(self.tab)
+        self.deleteFilmButton.setGeometry(QRect(200, 10, 151, 51))
+        self.deleteFilmButton.setObjectName("deleteFilmButton")
+        self.addFilmButton = QPushButton(self.tab)
+        self.addFilmButton.setGeometry(QRect(20, 10, 151, 51))
+        self.addFilmButton.setObjectName("addFilmButton")
+        self.filmsTable = QTableWidget(self.tab)
+        self.filmsTable.setGeometry(QRect(20, 80, 1181, 571))
+        self.filmsTable.setObjectName("filmsTable")
+        self.filmsTable.setColumnCount(0)
+        self.filmsTable.setRowCount(0)
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.addFilmButton_2 = QPushButton(self.tab_2)
+        self.addFilmButton_2.setGeometry( QRect(20, 10, 200, 51))
+        self.addFilmButton_2.setObjectName("addFilmButton_2")
+        self.deleteFilmButton_5 = QPushButton(self.tab_2)
+        self.deleteFilmButton_5.setGeometry( QRect(230, 10, 200, 51))
+        self.deleteFilmButton_5.setObjectName("deleteFilmButton_5")
+        self.filmsTable_5  = QTableWidget(self.tab_2)
+        self.filmsTable_5.setGeometry( QRect(20, 80, 1181, 571))
+        self.filmsTable_5.setObjectName("filmsTable_5")
+        self.filmsTable_5.setColumnCount(0)
+        self.filmsTable_5.setRowCount(0)
+        self.tabWidget.addTab(self.tab_2, "")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
+        self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.exitAction = QAction(MainWindow)
+        self.exitAction.setObjectName("exitAction")
 
         self.retranslateUi(MainWindow)
-
+        self.tabWidget.setCurrentIndex(1)
         QMetaObject.connectSlotsByName(MainWindow)
-    # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"\u0421\u0438\u0441\u0442\u0435\u043c\u0430 \u0441\u0443\u0434\u0435\u0439\u0441\u0442\u0432\u0430", None))
-        self.exitAction.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0445\u043e\u0434", None))
-        self.addFilmButton.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0437\u0430\u0435\u0437\u0434", None))
-        self.deleteFilmButton.setText(QCoreApplication.translate("MainWindow", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c \u0437\u0430\u0435\u0437\u0434", None))
+        _translate = QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "\u0421\u0438\u0441\u0442\u0435\u043c\u0430 \u0441\u0443\u0434\u0435\u0439\u0441\u0442\u0432\u0430"))
+        self.deleteFilmButton.setText(_translate("MainWindow", "\u0423\u0434\u0430\u043b\u0438\u0442\u044c \u0437\u0430\u0435\u0437\u0434"))
+        self.addFilmButton.setText(_translate("MainWindow", "\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0437\u0430\u0435\u0437\u0434"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "\u0417\u0430\u0435\u0437\u0434\u044b"))
+        self.addFilmButton_2.setText(_translate("MainWindow", "\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0441\u043e\u0440\u0435\u0432\u043d\u043e\u0432\u0430\u043d\u0438\u0435"))
+        self.deleteFilmButton_5.setText(_translate("MainWindow", "\u0423\u0434\u0430\u043b\u0438\u0442\u044c \u0441\u043e\u0440\u0435\u0432\u043d\u043e\u0432\u0430\u043d\u0438\u0435"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "\u0421\u043e\u0440\u0435\u0432\u043d\u043e\u0432\u0430\u043d\u0438\u044f"))
+        self.exitAction.setText(_translate("MainWindow", "Выход"))
+
     # retranslateUi
 
 
@@ -259,6 +286,76 @@ class Main(QMainWindow, Ui_MainWindow):  # Вот тут основное окн
 
     def exit(self):
         self.close()
+    #
+    #     def up_f2(self):
+    #         result2 = []
+    #         connection = pymysql.connect(
+    #             host='VH293.spaceweb.ru.',
+    #             port=3306,
+    #             user='savateevdm',
+    #             password='LJJ1C&xG3GW1Z53H',
+    #             database='savateevdm',
+    #             cursorclass=pymysql.cursors.DictCursor
+    #         )
+    #     with connection.cursor() as cursor:
+    #         select_all_rows = "SELECT * FROM `race`"
+    #         cursor.execute(select_all_rows)
+    #         res2 = cursor.fetchall()
+    #     connection.close()
+    #     cursor.close()
+    #
+    #     for i in res2:
+    #         res = list(i.values())
+    #         result2.append(res)
+    #     print(result2)
+    #
+    #     self.filmsTable2.setRowCount(len(result2))
+    #     self.filmsTable.setColumnCount(len(result2[0]))
+    #     self.filmsTable.setHorizontalHeaderLabels(
+    #         ['ID', 'Наименование соревнования', 'Дата', 'Наименование организатора', 'Место проведения'])
+    #
+    #     for i, elem in enumerate(result):
+    #         for j, val in enumerate(elem):
+    #             self.filmsTable.setItem(i, j, QTableWidgetItem(str(val)))
+    #
+    # def af(self):
+    #     dialog = AddRaceWidget(self)
+    #     dialog.show()
+    #
+    # def df(self):
+    #     rows = list(set([i.row() for i in self.filmsTable.selectedItems()]))
+    #     ids = [self.filmsTable.item(i, 0).text() for i in rows]
+    #     if not ids:
+    #         self.statusBar().showMessage('Выберите что-нибудь!')
+    #         return
+    #
+    #     else:
+    #         self.statusBar().showMessage('')
+    #     q = QMessageBox.question(self, '', 'Вы уверены, что хотите удалить этот элемент?',
+    #                                  QMessageBox.Yes,
+    #                                  QMessageBox.No)
+    #     if q == QMessageBox.Yes:
+    #         connection = pymysql.connect(
+    #             host='VH293.spaceweb.ru.',
+    #             port=3306,
+    #             user='savateevdm',
+    #             password='LJJ1C&xG3GW1Z53H',
+    #             database='savateevdm',
+    #             cursorclass=pymysql.cursors.DictCursor
+    #         )
+    #         print(int(''.join(ids)))
+    #
+    #         with connection.cursor() as cursor:
+    #             insert_query = f'''DELETE FROM `data` WHERE id = "{int(''.join(ids))}";'''
+    #             cursor.execute(insert_query)
+    #             connection.commit()
+    #             print('удалена')
+    #
+    #         connection.close()
+    #         cursor.close()
+    #         self.up_f()
+    #     cursor.close()
+
 
 
 if __name__ == '__main__':
